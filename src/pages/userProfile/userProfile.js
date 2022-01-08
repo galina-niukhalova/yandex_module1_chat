1a3d14fe35b286c737641f9df7e0fc989ba67355
Galina <galina.hvan1011@gmail.com>
2022-01-08 22:28:12 +1100

    user profile avatar

--- src/pages/userProfile/userProfile.js
+++ src/pages/userProfile/userProfile.js
@@ -1,8 +1,35 @@
 import userProfileTemplate from './userProfile.tmpl.hbs';
 import './userProfile.style.scss';
 
-function renderUserProfilePage() {
-  document.body.innerHTML = userProfileTemplate();
+function listenAvatarUpload() {
+  document.getElementsByClassName('profile__avatar-file-upload')[0]
+    .addEventListener('input', ({ target: input }) => {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        document.getElementsByClassName('profile__avatar-image')[0].src = e.target.result;
+      };
+
+      reader.readAsDataURL(input.files[0]);
+
+      if (input.files[0]) {
+        document.getElementsByClassName('profile__avatar-label')[0].classList.add('profile__avatar-label_hidden');
+        document.getElementsByClassName('profile__avatar-image')[0].classList.remove('profile__avatar-image_hidden');
+      }
+    });
 }
+
+
+function renderUserProfilePage() {
+  document.body.innerHTML = userProfileTemplate({
+    avatar: {
+      imageUrl: ""
+    },
+    userName: "Test user",
+  });
+
+  listenAvatarUpload();
+}
+
 
 export default renderUserProfilePage;
